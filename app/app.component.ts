264b77403c2057be7b7e5d6ed8b4ab18d4064bc2
Siva Karri <Siva.Karri@rci.rogers.ca>
2016-06-27 07:41:12 -0400

    Working code

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+   
+    selector: 'bus-route',
+    templateUrl: 'app/app.component.html',
+    styleUrls:['app/app.component.css']
+    
+})
+export class AppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
