6905557203baf593c9cc92f3301b1637d32013b2
Siva Karri <Siva.Karri@rci.rogers.ca>
2016-07-08 08:23:23 -0400

    gif addition

--- app/common/search.component.ts
+++ app/common/search.component.ts
@@ -8,8 +8,16 @@ import { Routes , ROUTER_DIRECTIVES , ROUTER_PROVIDERS} from'@angular/router';
      directives:[ROUTER_DIRECTIVES] 
 })
 export class SearchComponent implements OnInit {
+    public flip=true;
     constructor() { }
 
     ngOnInit() { }
+    public flipmethod(){
+        if(this.flip){
+            this.flip=false;
+        }else{
+        this.flip=true;
+        }
+    }
 
 }
