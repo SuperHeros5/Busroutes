6b3b3395e027abd7283ca262d35b613039b33213
ksivaganesh99 <ksivaganesh99@gmail.com>
2016-07-11 23:05:11 +0530

    Added bus image

--- app/shared/config.ts
+++ app/shared/config.ts
@@ -1,10 +1,16 @@
 export let CONFIG={
     baseurls:{
-        allbuses:"http://vdesntcs0587:8095/sample/restApi/helloworld/place?place=Gachibowli Junction",
+      /*  allbuses:"http://vdesntcs0587:8095/sample/restApi/helloworld/place?place=Gachibowli Junction",
         updatebus:"http://vdesntcs0587:8095/sample/restApi/helloworld/place?place=Gachibowli Junction",
         deletebus:"http://vdesntcs0587:8095/sample/restApi/helloworld/place?place=Gachibowli Junction",
         getplaces:"http://vdesntcs0587:8095/sample/restApi/helloworld/xml",
-        getselectedplace:"http://vdesntcs0587:8095/sample/restApi/helloworld/place"
+        getselectedplace:"http://vdesntcs0587:8095/sample/restApi/helloworld/place"*/
+        
+         allbuses:"http://localhost:8092/AngularApp/restApi/helloworld/place?place=Gachibowli Junction",
+        updatebus:"http://localhost:8092/AngularApp/restApi/helloworld/place?place=Gachibowli Junction",
+        deletebus:"http://localhost:8092/AngularApp/restApi/helloworld/place?place=Gachibowli Junction",
+        getplaces:"http://localhost:8092/AngularApp/restApi/helloworld/xml",
+        getselectedplace:"http://localhost:8092/AngularApp/restApi/helloworld/place"
     }
     
     
