f11de6135fffb4803a7394bddb7d30cb16f10597
ksivaganesh99 <ksivaganesh99@gmail.com>
2016-09-23 18:12:12 +0530

    busservice to fetch weather details
    busservice to fetch weather details

--- app/buses/buses.component.ts
+++ app/buses/buses.component.ts
@@ -18,6 +18,7 @@ export class BusesComponent implements OnInit,OnDestroy,OnChanges {
    public buses;
     public sourcevalue;
     public busessources;
+    public weatherdata;
     public srcadi:boolean = true;
     public srcloc:boolean = true;
     public destadi:boolean = true;
@@ -66,8 +67,11 @@ export class BusesComponent implements OnInit,OnDestroy,OnChanges {
         this.destadi=true;
         this.srcloc=false;
         document.getElementById("destplace").value="Adibatla";
-        this._busservices.getToAdibatla(selectedvalue)
-     .subscribe(data => {this.buses=data;console.log(data);},error => console.log(error),()=>console.log("completed"));
+           this._busservices.getToAdibatla(selectedvalue)
+     .subscribe(data => {this.buses=data;console.log(data);},error => console.log(error),()=>{console.log("completed")
+ this._busservices.getWeather("17.236","78.54")
+    .subscribe(data => {this.weatherdata=data;console.log(data);},error => console.log(error),()=>console.log("weater fetched"));    
+});
        this.sourcevalue=selectedvalue;
     }
      document.getElementById("destplace").focus();
@@ -78,8 +82,12 @@ export class BusesComponent implements OnInit,OnDestroy,OnChanges {
       let places=document.getElementById("destplace");
     let selectedvalue = places.options[places.selectedIndex].innerHTML;
    this.sourcevalue=selectedvalue;
-     this._busservices.getFromAdibatla(selectedvalue)
-     .subscribe(data => {this.buses=data;console.log(data);},error => console.log(error),()=>console.log("completed"));
+    this._busservices.getFromAdibatla(selectedvalue)
+     .subscribe(data => {this.buses=data;console.log(data);},error => console.log(error),()=>{
+         console.log("completed");
+       this._busservices.getWeather(this.buses.lattitude,this.buses.longitude)
+    .subscribe(data => {this.weatherdata=data;console.log(data);},error => console.log(error),()=>console.log("weater fetched"));
+     });
        
         }
 }
