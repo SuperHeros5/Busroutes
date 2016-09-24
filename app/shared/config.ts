5df82477db1d7a45880689def3be4f64de9b2acf
ksivaganesh99 <ksivaganesh99@gmail.com>
2016-09-24 05:26:06 +0000

    https url added
    https url added

--- app/shared/config.ts
+++ app/shared/config.ts
@@ -3,13 +3,13 @@ export let CONFIG={
         allbuses:"http://busroutesip.no-ip.org:8080/BusRoutesApi/restApi/helloworld/place?place=Gachibowli Junction",
         updatebus:"http://vdesntcs0587:8095/sample/restApi/helloworld/place?place=Gachibowli Junction",
         deletebus:"http://vdesntcs0587:8095/sample/restApi/helloworld/place?place=Gachibowli Junction",
-      /*  getplaces:"https://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8443/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/xml",
+        getplaces:"https://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8443/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/xml",
         gettoadibatlaplaces:"https://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8443/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/toadibatla",
-        getfromadibatlaplaces:"https://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8443/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/fromadibatla"*/
+        getfromadibatlaplaces:"https://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8443/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/fromadibatla"
         
-         getplaces:"https://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8080/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/xml",
-        gettoadibatlaplaces:"https://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8080/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/toadibatla",
-        getfromadibatlaplaces:"https://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8080/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/fromadibatla",
+         /*getplaces:"http://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8080/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/xml",
+        gettoadibatlaplaces:"http://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8080/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/toadibatla",
+        getfromadibatlaplaces:"http://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8080/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/fromadibatla",*/
         getweather:"https://api.darksky.net/forecast/61bc1053957b98072cf039ac033cabc9"
       /*   allbuses:"http://localhost:8092/AngularApp/restApi/helloworld/place?place=Gachibowli Junction",
         updatebus:"http://localhost:8092/AngularApp/restApi/helloworld/place?place=Gachibowli Junction",
