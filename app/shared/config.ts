db6ff6f7fed13dc5efd71c997b17c9b0c0a4c28a
ksivaganesh99 <ksivaganesh99@gmail.com>
2016-10-29 15:58:54 +0530

    Update config.ts

--- app/shared/config.ts
+++ app/shared/config.ts
@@ -12,7 +12,7 @@ export let CONFIG={
         getfromadibatlaplaces:"http://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8080/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/fromadibatla",*/
         
          getplaces:"https://sleet-shoulder.hyperdev.space/api/bears",
-        gettoadibatlaplaces:"http://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8080/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/toadibatla",
+        gettoadibatlaplaces:"https://sleet-shoulder.hyperdev.space/api/bears/",
         getfromadibatlaplaces:"http://ec2-52-66-113-33.ap-south-1.compute.amazonaws.com:8080/BusRoutesApi-0.0.1-SNAPSHOT/restApi/helloworld/fromadibatla",
        // getweather:"https://api.darksky.net/forecast/61bc1053957b98072cf039ac033cabc9"
           getweather:"http://api.apixu.com/v1/current.json?key=d0f9731bb63b4d4db0372112162609&"
